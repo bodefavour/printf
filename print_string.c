89e32d4df9cac4817e8b4ff7f25545480247d10b
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-09-21 20:25:08 +0000

    completed task 1, check

---
+++ print_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_string - puts characters on the screen
+ *
+ * @str: string to be printed
+ *
+ * Return: the number of characters printed
+ */
+
+int print_string(const char *str)
+{
+	unsigned int looper, nos = 0;
+
+	for (looper = 0; str[looper] != '\0'; looper++)
+	{
+		_putchar(str[looper]);
+		nos++;
+	}
+	return (nos);
+}
